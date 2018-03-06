d03cbf1b959fc9a065cf5ea5a0d2f1a6cd6bbf2f
salihayildiz <36633211+salihayildiz@users.noreply.github.com>
2018-03-07 01:56:41 +0300

    Add files via upload

---
+++ Dikdörtgen Alan Hesapla.py
@@ -0,0 +1,9 @@
+#dortgen_alan_hesapla_v1
+
+a = input("A kenarının uzunluğunu girin:  ")
+
+b = input("B kenarının uzunluğunu girin: ")
+
+alan = int(a)*int(b)
+
+print ("Alan = " ,alan)
---
+++ Dörtgen ve daire alanı.py
@@ -0,0 +1,14 @@
+def dortgen_alan_hesapla_vl(a,b):
+    alan = a*b
+    print("alan=", alan)
+a = int(input("Dikdörtgenin kısa kenarı:"))
+b = int(input("Dikdörtgenin uzun kenarı:"))
+dortgen_alan_hesapla_vl(a,b)
+
+def daire_alan_hesapla_v1(pisayisi,r):
+    alan = pisayisi*(r**2)
+    print("alan=", alan)
+pisayisi= 3.14
+r= int(input("Dairenin yarıçapı="))
+daire_alan_hesapla_v1(pisayisi,r)
+
